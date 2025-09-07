7c266d24ea81bc225e2664a1c73f140a55912f30
Valkenan <adamfennych07@gmail.com>
2025-09-07 15:00:09 +0200

    add cours 6 LESSON_21_AUTOMATIC_VARIABLES

---
+++ LESSON_21_AUTOMATIC_VARIABLES/LESSON_21_AUTOMATIC_VARIABLES.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    auto x = 10;
+    auto y = 12.5;
+    auto z = "Mohamed Abu-Hadhoud";
+
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
+
+    return 0;
+}
