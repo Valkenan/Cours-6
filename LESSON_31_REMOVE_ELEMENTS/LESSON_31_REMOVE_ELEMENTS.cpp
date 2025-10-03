98382db441a5dee4854808a7d4e312f57598f80c
Valkenan <adamfennych07@gmail.com>
2025-10-03 21:36:22 +0100

    add cours 6 LESSON_32_VECTOR_FUNCTIONS

--- LESSON_31_REMOVE_ELEMENTS/LESSON_31_REMOVE_ELEMENTS.cpp
+++ LESSON_31_REMOVE_ELEMENTS/LESSON_31_REMOVE_ELEMENTS.cpp
@@ -1,20 +1,33 @@
-// LESSON_31_REMOVE_ELEMENTS.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <vector>
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<int> vNumbers;
+
+    vNumbers.push_back(10);
+    vNumbers.push_back(20);
+    vNumbers.push_back(30);
+    vNumbers.push_back(40);
+    vNumbers.push_back(50);
+    //cout << vNumbers.size() << endl;how many elements in the stack
+    //vNumbers.clear();clear the stack
+    vNumbers.pop_back();
+    vNumbers.pop_back();
+    vNumbers.pop_back();
+    vNumbers.pop_back();
+    vNumbers.pop_back();
+
+    cout << "Numbers Vector: \n\n";
+
+    // ranged loop
+    for (int& Number : vNumbers) {
+        cout << Number << endl;
+    }
+
+    cout << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ LESSON_32_VECTOR_FUNCTIONS/LESSON_32_VECTOR_FUNCTIONS.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    vector<int> vNumbers;
+
+    vNumbers.push_back(10);
+    vNumbers.push_back(20);
+    vNumbers.push_back(30);
+    vNumbers.push_back(40);
+    vNumbers.push_back(50);
+
+    cout << "First Element : " << vNumbers.front() << endl;
+    cout << "Last Element :" << vNumbers.back() << endl;
+    cout << "Size : " << vNumbers.size() << endl;
+    cout << "Capacity : " << vNumbers.capacity() << endl;
+    cout << "Empyt : " << vNumbers.empty() << endl;//if empty = 1
+    return 0;
+}
