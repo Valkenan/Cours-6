fa03c20eb686e6025a316de1817f131790ef2af2
Valkenan <adamfennych07@gmail.com>
2025-10-05 16:44:00 +0100

    add cours 6 LESSON_43_MEMORY_MANAGEMENT_NEW_AND_DELETE

---
+++ LESSON_43_MEMORY_MANAGEMENT_NEW_AND_DELETE/LESSON_43_MEMORY_MANAGEMENT_NEW_AND_DELETE.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	// declare an int Pointer
+	int* ptrX;
+	// declare a float Pointer
+	float* ptrY;
+	// dynamically allocate memory
+	// إنشاء متغير مؤقت في الذاكرة
+	ptrX = new int;
+	ptrY = new float;
+	// assigning value to the memory
+	// إضافة قيمة للمتغير المؤقت في الذاكرة
+	*ptrX = 45;
+	*ptrY = 58.35;
+	cout << *ptrX << endl;
+	cout << *ptrY << endl;
+	// deallocate the memory
+	// إزالة المتغير المؤقت من الذاكرة
+	delete ptrX;
+	delete ptrY;
+	cout << endl;
+}
